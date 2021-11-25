c0a8a29091870a0d252b8290c03bda0655178c57
Ajenkris Kungkung <kristt26@gmail.com>
2021-11-25 18:32:57 +0900

    poksd

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,19 +1,19 @@
         <?= $this->extend('layout/layout') ?>
         <?= $this->section('content') ?>
-       <!-- Container Fluid-->
+        <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-            </ol>
-          </div>
-          
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                </ol>
+            </div>
+
 
         </div>
-        <script src="<?=base_url()?>/public/template/js/demo/chart-area-demo.js"></script>
-        <script src="<?=base_url()?>/public/template/js/demo/chart-pie-demo.js"></script>
-        <script src="<?=base_url()?>/public/template/js/demo/chart-bar-demo.js"></script>
+        <script src="<?=base_url()?>/template/js/demo/chart-area-demo.js"></script>
+        <script src="<?=base_url()?>/template/js/demo/chart-pie-demo.js"></script>
+        <script src="<?=base_url()?>/template/js/demo/chart-bar-demo.js"></script>
         <!---Container Fluid-->
         <?= $this->endSection() ?>
