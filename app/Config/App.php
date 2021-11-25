afea03c4964246a60fb65179d090d776f5c05860
Ajenkris Kungkung <kristt26@gmail.com>
2021-11-25 18:06:54 +0900

    as

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = base;
+	public $baseURL = BASE;
 
 	/**
 	 * --------------------------------------------------------------------------
